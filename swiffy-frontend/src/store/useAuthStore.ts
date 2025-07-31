3e2b0590a29a3b33227e6b2ebbbbb68e3a6d9899
GitPersSiham <44850768+GitPersSiham@users.noreply.github.com>
2025-07-31 12:29:52 +0200

    Update useAuthStore.ts

--- swiffy-frontend/src/store/useAuthStore.ts
+++ swiffy-frontend/src/store/useAuthStore.ts
@@ -1,29 +1,3 @@
-// stores/useAuthStore.ts
-/*import { create } from 'zustand';
-import { User } from "@/types/User"
-
-
-type AuthState = {
-  isAuthenticated: boolean,
-  user: User | null;
-  login: (credentials: User) => void; // Définition de la fonction login
-  logout: () => void;
-};
-
-export const useAuthStore = create<AuthState>((set) => ({
-  isAuthenticated: false,
-  user: null,
-  login: (credentials: any) => {
-    set({ isAuthenticated: true, user: {  email: credentials.email, id: credentials.userId , name:credentials.name } });
-   
-  },
-  register: (userData : any) => {
-    console.log('Inscription avec :', userData);
-    set({ isAuthenticated: true, user: { email: userData.email, id: userData.userId , name:userData.name } });
-  },
-  logout: () => set({ isAuthenticated: false, user: null }),
-}));
-*/
 import { create } from 'zustand'
 
 interface AuthState {
